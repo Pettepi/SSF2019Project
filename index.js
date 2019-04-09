ea67f2addeedc930337e1d5ae2e6541bd01cc2d0
pp <petteripiironen@Petteri-MacBook-Pro.local>
2019-04-09 12:33:42 +0300

    created SSL security and added security

--- index.js
+++ index.js
@@ -6,13 +6,25 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/';
+const fs = require('fs');
 require('dotenv').config();
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+// SSL
+const sslkey = fs.readFileSync('ssl-key.pem');
+const sslcert = fs.readFileSync('ssl-cert.pem');
 
-mongoose.connect(url, {useNewUrlParser: true}).then(()=>{
+const options = {
+    key: sslkey,
+    cert: sslcert,
+};
+
+// MongoDB server initialization
+mongoose.connect(url, options, {useNewUrlParser: true}).then(()=>{
     console.log('connected succesfully');
+    app.get('/', (req, res) => {
+        res.send('Security works')
+    });
     app.listen(3000);
 }, err => {
     console.log('connection to database failed' + err);

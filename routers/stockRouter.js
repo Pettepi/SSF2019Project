61941bddb94791c1bd6ba240bd0021e165f59198
pp <petteripiironen@Petteri-MacBook-Pro.local>
2019-05-10 14:23:31 +0300

    minor updates

--- routers/stockRouter.js
+++ routers/stockRouter.js
@@ -4,16 +4,17 @@ const fetch = require('node-fetch');
 const symbol = 'MSFT';
 const apiurl = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol='+symbol+'&interval=5min&apikey='+process.env.api_key;
 
+/*
 const pos = apiurl.lastIndexOf(symbol);
-
+/*
 const input = document.createElement("INPUT");
 input.setAttribute("type", "text");
 
 let userinput = input;
 // replace api query symbol string with that of users input
-
+// jsdom not working in Node.js!!!!
 pos.replace("symbol", userinput);
-
+*/
 
 router.get('/', (req, res) => {
     fetch(apiurl)
